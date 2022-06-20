8c17174f8e919fa2ef639f9fce9f4639ba4c9cba
Jeremy L. Shepherd <jeremylshepherd@gmail.com>
2022-06-19 20:44:43 -0400

    - Updated test to verify that `start.html` page loads

--- src/test/java/com/uc_it4045/assignment_tracker/AssignmentTrackerApplicationTests.java
+++ src/test/java/com/uc_it4045/assignment_tracker/AssignmentTrackerApplicationTests.java
@@ -1,13 +1,19 @@
 package com.uc_it4045.assignment_tracker;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AssignmentTrackerApplicationTests {
 
+	@Autowired
+	private AssignmentTrackerController controller;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
