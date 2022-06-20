b52196d1cc3e606140f06629284bac9cd8c0d406
Jeremy L. Shepherd <jeremylshepherd@gmail.com>
2022-06-19 20:23:19 -0400

    - Added dependencies for Thymeleaf and Lombok - Created `AssignmentTrackerController` and mapped Index route to `start.html` - Created `start.html`

---
+++ src/main/java/com/uc_it4045/assignment_tracker/AssignmentTrackerController.java
@@ -0,0 +1,15 @@
+package com.uc_it4045.assignment_tracker;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AssignmentTrackerController {
+        /**
+         * Handle the / endpoint
+         * @return Start page template
+         */
+        @RequestMapping("/")
+        public String index() {
+            return "start";
+        }
+    }
