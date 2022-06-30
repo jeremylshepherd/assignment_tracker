4fbded8d9106aa01b206c3af742486a6ee6532a7
bartrn <89704134+bartrn@users.noreply.github.com>
2022-06-30 13:54:28 -0400

    to fix one of the errors of the project

--- src/main/java/com/uc_it4045/assignment_tracker/AssignmentTrackerApplication.java
+++ src/main/java/com/uc_it4045/assignment_tracker/AssignmentTrackerApplication.java
@@ -3,11 +3,16 @@ package com.uc_it4045.assignment_tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static org.springframework.boot.SpringApplication.*;
+
 @SpringBootApplication
 public class AssignmentTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AssignmentTrackerApplication.class, args);
+		run(AssignmentTrackerApplication.class, args);
+	}
+//added the code here to fix the error
+	private static void run(java.lang.Class aClass, String[] args) {
 	}
 
 }
