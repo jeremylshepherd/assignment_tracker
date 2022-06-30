a6b70633629f6dc723bb7d8899532ad2f41b5b18
bartrn <89704134+bartrn@users.noreply.github.com>
2022-06-30 14:30:20 -0400

    editing controller to allow you to get your assignment
    fill in the "" with the your correct variables once your able to

--- src/main/java/com/uc_it4045/assignment_tracker/UserController/AssignmentTrackerController.java
+++ src/main/java/com/uc_it4045/assignment_tracker/UserController/AssignmentTrackerController.java
@@ -1,6 +1,8 @@
 package com.uc_it4045.assignment_tracker.UserController;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AssignmentTrackerController {
@@ -12,4 +14,11 @@ public class AssignmentTrackerController {
         public String index() {
             return "start";
         }
+        
+        @GetMapping("/")
+        public GetAssignment<"Assignment">("/"){
+            return "assignment";
+        }
+
+
     }
