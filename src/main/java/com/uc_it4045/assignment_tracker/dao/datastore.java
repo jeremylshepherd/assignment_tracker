d249ec9d266327d821f92c4c3bbb5e2ac194504d
Robert Champion <champirr@mail.uc.edu>
2022-07-17 12:26:22 -0400

    Create datastore.java

---
+++ src/main/java/com/uc_it4045/assignment_tracker/dao/datastore.java
@@ -0,0 +1,23 @@
+package com.uc_it4045.assignment_tracker.dao;
+
+
+class User{
+    private String firstName;
+    private String lastName;
+    User(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public String frstName(){
+        return firstName;
+    }
+    public String lstName(){
+        return lastName;
+    }
+
+}
+class datastore{
+public static void main(String[] args){
+
+}
+}
