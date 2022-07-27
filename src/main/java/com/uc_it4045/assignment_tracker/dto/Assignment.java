22010b66144c137139d5d2c9ad0cc3fefbe9e008
bartrn <89704134+bartrn@users.noreply.github.com>
2022-07-27 16:40:33 -0400

    created Assignment Dto

---
+++ src/main/java/com/uc_it4045/assignment_tracker/dto/Assignment.java
@@ -0,0 +1,14 @@
+package com.uc_it4045.assignment_tracker.dto;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Assignment {
+    int Id;
+    String Title;
+    String Description;
+    double Points;
+    String User;
+    String Date;
+}
