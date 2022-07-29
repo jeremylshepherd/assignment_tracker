239400c5178ab106f7b79a05ed9600b398692b2e
Jaspreet Singh <71667958+Singh2je@users.noreply.github.com>
2022-07-29 14:17:20 -0400

    code review branch commit.

--- src/main/java/com/uc_it4045/assignment_tracker/dto/User.java
+++ src/main/java/com/uc_it4045/assignment_tracker/dto/User.java
@@ -1,24 +1,11 @@
 package com.uc_it4045.assignment_tracker.dto;
 
+import lombok.Data;
+
 import java.util.List;
 
-public class User {
+public @Data class User {
     String firstName;
     String lastName;
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
--- src/test/java/com/uc_it4045/assignment_tracker/AssignmentTrackerApplicationTests.java
+++ src/test/java/com/uc_it4045/assignment_tracker/AssignmentTrackerApplicationTests.java
@@ -1,6 +1,7 @@
 package com.uc_it4045.assignment_tracker;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,12 +9,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AssignmentTrackerApplicationTests {
 
-	@Autowired
-	private AssignmentTrackerController controller;
+    @Autowired
+    private AssignmentTrackerController controller;
 
-	@Test
-	public void contextLoads() throws Exception {
-		assertThat(controller).isNotNull();
-	}
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }
 
 }
