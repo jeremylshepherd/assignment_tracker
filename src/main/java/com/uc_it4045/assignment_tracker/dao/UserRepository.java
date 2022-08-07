563c03bcf7ed431d2055bb65fa8c089deb23e83e
Jeremy L. Shepherd <jeremylshepherd@gmail.com>
2022-08-06 20:49:30 -0400

    Add negative annotation for circleci

--- src/main/java/com/uc_it4045/assignment_tracker/dao/UserRepository.java
+++ src/main/java/com/uc_it4045/assignment_tracker/dao/UserRepository.java
@@ -1,10 +1,12 @@
 package com.uc_it4045.assignment_tracker.dao;
 
 import com.uc_it4045.assignment_tracker.dto.AuthUser;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Profile("!test")
 public interface UserRepository extends CrudRepository<AuthUser, Integer> {
     AuthUser findByUsername(String username);
 }
